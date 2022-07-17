7cf07a044019bc305115dd1a84fd4a4ea6dbd4a9
Neel <neelnarayanshetty@gmail.com>
2022-07-17 18:59:22 +0530

    holiday

---
+++ dynamicProgramming/coin.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//bottom up dp
+int minCoinsForChange(int m,vector<int> denoms)
+{
+    vector<int> dp(m+1,0);
+    dp[0]=0;
+
+    for(int i=1;i<=m;i++)
+    {
+        dp[i] = INT_MAX;
+        for(int c : denoms )
+        {
+            if(i - c>=0 and dp[i-c]!=INT_MAX)
+            {
+                dp[i]=min(dp[i],dp[i-c]+1);
+            }
+        }
+
+    }
+    return dp[m]==INT_MAX? -1 : dp[m];
+}
+
+int main()
+{
+    vector<int> denoms = {1,5,7,10};
+    int m = 16;
+    cout<<minCoinsForChange(m,denoms);
+}
